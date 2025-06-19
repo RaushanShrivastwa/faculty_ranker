7e09c6f0dfc6c18eafdacb915d79eeafb06ec3cc
hmm183 <119665141+hmm183@users.noreply.github.com>
2025-06-19 17:12:58 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,4 +1,4 @@
-// src/components/Signup.jsx
+// src/components/Signup.jsx capital
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Signup.css';
