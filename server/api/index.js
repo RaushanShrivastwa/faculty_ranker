8ff3e01490df931b7abf26a7a28acbcf8f25bab1
hmm183 <119665141+hmm183@users.noreply.github.com>
2025-06-20 23:56:53 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,7 @@
-// server/api/index.js
+// server/functions/api.js
 const serverless = require('serverless-http');
-const app        = require('../index');   // pulls in server/index.js
-module.exports = serverless(app);
+const app = require('../index'); // full Express app with routes
+
+module.exports = serverless(app, {
+  callbackWaitsForEmptyEventLoop: false,
+});
